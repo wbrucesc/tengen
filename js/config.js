4c8b7540d0939375acf7142b051f587f43fb23ea
Will Bruce <willtiger13@gmail.com>
2026-06-12 13:59:56 -0400

    chore: add Supabase credentials to enable online play

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Online multiplayer credentials.
 // Create a free project at supabase.com, then follow SUPABASE_SETUP.md.
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://xxnqzdmargxiesmwbhaq.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4bnF6ZG1hcmd4aWVzbXdiaGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzUyMTIsImV4cCI6MjA5Njg1MTIxMn0.SO7OnaK8VcG7ZzWSbe2TMZbo8HAJrtDTQSDWxqnpRbs";
